55e58a7501e43ee143610454a07f3ca40cd831d7
Yejin Lee(이예진) <111052302+ImxYJL@users.noreply.github.com>
2025-03-03 15:21:55 +0900

    [Gold IV] Title: 부분합, Time: 192 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+
+const [n, s] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+let startIdx = 0;
+let endIdx = 0;
+
+const ps = Array.from({length: n}, () => 0);
+ps[0] = arr[0];
+for(let i=1; i<arr.length; i++)
+{
+     ps[i] = arr[i] + ps[i-1];
+}
+const calculateSum = (startIdx, endIdx) => {  
+    return ps[endIdx] - (startIdx > 0 ? ps[startIdx - 1] : 0);
+};
+
+let isPossible = false;
+let minLen = arr.length;
+
+// 불가능하다면 0을 출력한다.
+while(startIdx < arr.length && endIdx < arr.length){ // 조건수정하기 ㅋㅋㅋ
+    if(calculateSum(startIdx, endIdx) >= s){
+        minLen = Math.min(minLen, endIdx-startIdx+1);
+        startIdx++;
+        isPossible = true;
+    }  else{
+        endIdx++;
+    }      
+}
+
+console.log(isPossible? minLen: 0);
