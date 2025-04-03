5a701c9a21b7fbe2323f2bcd2435e4d9443297e4
Yejin Lee(이예진) <111052302+ImxYJL@users.noreply.github.com>
2025-04-03 19:11:02 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 72 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,20 +1,13 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-n, k = map(int, input().strip().split())
-q = deque()
+n, k = map(int, input().split())
+q = deque(range(1, n + 1))
 ans = []
 
-for i in range(1, n+1):
-    q.append(i)
-
 while q:
-    cnt = k
-    while cnt > 1 :
+    rotate = (k - 1) % len(q)
+    for _ in range(rotate):
         q.append(q.popleft())
-        cnt -= 1
-        
     ans.append(q.popleft())
 
 print("<" + ", ".join(map(str, ans)) + ">")
