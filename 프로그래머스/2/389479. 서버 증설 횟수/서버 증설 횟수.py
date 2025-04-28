f8fa163becbbedd6985638427c35261a90f4031b
Yejin Lee(이예진) <111052302+ImxYJL@users.noreply.github.com>
2025-04-28 23:07:20 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -0,0 +1,25 @@
+def solution(players, m, k):
+    answer = 0
+    cur_server = 0
+    server_off = [0] * (len(players) + k + 1)
+
+    for t, n in enumerate(players):
+        # 1. t 시점에서, 반납할 서버가 있으면 먼저 반납
+        cur_server -= server_off[t]
+
+        # 2. 현재 서버로 커버 가능한 수 계산
+        available = cur_server * m
+
+        # 3. 부족하면 서버 추가
+        required_server = n // m
+        alive_server = cur_server
+
+        if required_server > alive_server:
+            add = required_server - alive_server
+            cur_server += add
+            answer += add
+
+            # 4. 추가 서버는 t+k 시점에 반납
+            server_off[t + k] += add
+
+    return answer
