e3fe90fa98dbfe1a81d2ee4d210821f08212bcae
Yejin Lee(이예진) <111052302+ImxYJL@users.noreply.github.com>
2025-10-09 22:51:06 +0900

    [Gold IV] Title: 점수따먹기, Time: 1096 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.py
@@ -0,0 +1,40 @@
+import sys 
+from itertools import combinations
+input = sys.stdin.readline
+
+n, m = map(int, input().strip().split())
+arr = [[0] * (m+1)]
+for _ in range(n):
+    row = [0] + list(map(int, input().split()))
+    arr.append(row)
+    
+sum = [[0] * (m+1) for _ in range(n+1)]
+
+def kadane(array):
+    if not array:
+        return 0
+
+    curSum = array[1]
+    maxSum = array[1]
+
+    for i in range(2, len(array)):
+        num = array[i]
+        
+        curSum = max(curSum + num, num)
+        maxSum = max(curSum, maxSum)
+    
+    return maxSum
+
+
+maxSum = -float('inf')
+for leftCol in range(1, m+1):
+    curRowSum = [0] * (n+1)
+
+    for rightCol in range(leftCol, m+1):
+        for i in range(1, n+1):
+            curRowSum[i] += arr[i][rightCol]
+            
+        curSum = kadane(curRowSum)
+        maxSum= max(maxSum, curSum)
+            
+print(maxSum)
